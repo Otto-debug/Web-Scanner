13bc7db7ef237f19c462c9b6ef368219b7d16633
Otto-debug <ottolumbert@gmail.com>
2025-06-10 16:17:58 +0300

    (update): Конфигурация логгера

--- scanner/logger.py
+++ scanner/logger.py
@@ -1,25 +1,29 @@
 import logging
+import os
 
-def setup_logger():
+def setup_logger(name='web_scanner', log_file='logs/scanner.log'):
     """Логгер"""
-    logger = logging.getLogger('web_scanner')
+    logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    # Обработчик для файла
-    file_handler = logging.FileHandler('logs/scanner.log')
-    file_handler.setLevel(logging.INFO)
+    if not logger.handlers:
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
-    # Обработчик для консоли
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
+        # Обработчик для файла
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.INFO)
 
-    # Формат ввода
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+        # Обработчик для консоли
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
 
-    # Добавляем обработчик
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+        # Формат ввода
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+
+        # Добавляем обработчик
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
     return logger
